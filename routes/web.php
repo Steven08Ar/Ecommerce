22df96ca7069b43bbc899419d0f648c4de48a41d
Santiago Arias <steven08santi@gmail.com>
2025-09-09 13:51:36 -0500

    Rutas Basicas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    return view('products');
+})->name('products');
+
+Route::get('/products/{id}/{category?}', function ($id, $category = null) {
+
+    if ($category != null) {
+
+        return 'Details products: ' . $id . ' Category: ' . $category;
+    } else {
+        return 'Details products: ' . $id;
+    }
+})->name('product');
