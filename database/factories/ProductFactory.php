0ff0f26070262cc80fc5de7963991d8f31d0c8cc
Santiago Arias <steven08santi@gmail.com>
2025-10-14 13:35:20 -0500

    Product Factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ * 
  */
 class ProductFactory extends Factory
 {
