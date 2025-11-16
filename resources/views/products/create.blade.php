05e721a144ba3bd9445c6b8f6f9fc58f5aec2412
Santiago Arias <steven08santi@gmail.com>
2025-11-16 13:08:01 -0500

    Validaciones Formulario Products

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -13,26 +13,42 @@
                             @csrf
                             <div class="mb-3">
                                 <label for="productName" class="form-label">Nombre del producto</label>
-                                <input type="text" class="form-control" id="productName" name="productName"
-                                    placeholder="Ej: Laptop Gamer" required>
+                                @error('name')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                                <input type="text" class="form-control" id="productName" name="name"
+                                    placeholder="Ej: Laptop Gamer" value="{{ old('name') }}" required>
                             </div>
                             <div class="mb-3">
                                 <label for="productDescription" class="form-label">Descripción</label>
-                                <textarea class="form-control" id="productDescription" name="productDescription"
-                                    rows="4" placeholder="Escribe una descripción breve" required></textarea>
+                                @error('description')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                                <textarea class="form-control" id="productDescription" rows="4" name="description"
+                                    placeholder="Escribe una descripción breve" required>{{ old('description') }}</textarea>
                             </div>
                             <div class="row">
                                 <div class="col-md-6 mb-3">
                                     <label for="productPrice" class="form-label">Precio</label>
-                                    <input type="number" class="form-control" id="productPrice" name="productPrice"
-                                        placeholder="Ej: 3499000" min="0" step="1000" required>
+                                    @error('price')
+                                        <small class="text-danger">{{ $message }}</small>
+                                    @enderror
+                                    <input type="number" class="form-control" id="productPrice" name="price"
+                                        placeholder="Ej: 3499000" min="0" step="1000" value="{{ old('price') }}"
+                                        required>
                                 </div>
                                 <div class="col-md-6 mb-3">
                                     <label for="productCategory" class="form-label">Categoría</label>
-                                    <select class="form-select" id="productCategory" name="category" required>
-                                        <option value="">Seleccione una categoría</option>
-                                        @foreach ($categories as $category)
-                                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                    @error('category')
+                                        <small class="text-danger">{{ $message }}</small>
+                                    @enderror
+                                    <select class="form-control" id="productCategory" name="category">
+                                        <option value="">-- Category --</option>
+                                        @foreach ($categories as $item)
+                                            <option value="{{ $item->id }}"
+                                                {{ old('category') == $item->id ? 'selected' : '' }}>
+                                                {{ $item->name }}
+                                            </option>
                                         @endforeach
                                     </select>
                                 </div>
@@ -40,10 +56,15 @@
                             <div class="row">
                                 <div class="col-md-6 mb-3">
                                     <label for="productBrand" class="form-label">Marca</label>
-                                    <select class="form-select" id="productBrand" name="productBrand" required>
-                                        <option value="">Seleccione una marca</option>
-                                        @foreach ($brands as $brand)
-                                            <option value="{{ $brand->id }}">{{ $brand->name }}</option>
+                                    @error('brand')
+                                        <small class="text-danger">{{ $message }}</small>
+                                    @enderror
+                                    <select class="form-control" id="productBrand" name="brand">
+                                        <option value="">-- Brand --</option>
+                                        @foreach ($brands as $item)
+                                            <option value="{{ $item->id }}" {{ old('brand') == $item->id ? 'selected' : '' }}>
+                                                {{ $item->name }}
+                                            </option>
                                         @endforeach
                                     </select>
                                 </div>
